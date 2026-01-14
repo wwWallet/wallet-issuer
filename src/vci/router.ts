067b73d1c0b5aed4207f4bebf2b8a33a41dbf858
kkmanos <koukoularisman@gmail.com>
2026-01-14 15:11:10 +0200

    handle thrown errors in openid router

--- src/vci/router.ts
+++ src/vci/router.ts
@@ -6,56 +6,86 @@ export const vciRouter = Router();
 
 
 vciRouter.get('/credential-offer/:id', async (req, res) => {
-	const id = req.params.id as string;
-	if (!id) {
-		res.status(400).send({ error: "Not found" });
+	try {
+		const id = req.params.id as string;
+		if (!id) {
+			res.status(400).send({ error: "Not found" });
+		}
+		const obj = await issuer.getCredentialOffer(id, true);
+		res.status(200).send(obj);
 	}
-	const obj = await issuer.getCredentialOffer(id, true);
-	res.status(200).send(obj);
+	catch (e) {
+		logger.error(JSON.stringify(e));
+		res.status(500).send({ error: "internal_server_error" });
+	}
 });
 
 vciRouter.post('/nonce', async (_req, res) => {
-	const response = await issuer.issueNonce();
-	Object.entries(response.headers).map(([k, v]) => res.setHeader(k, v));
-	res.status(response.status).send(response.data);
-	logger.info("New c_nonce issued");
+	try {
+		const response = await issuer.issueNonce();
+		Object.entries(response.headers).map(([k, v]) => res.setHeader(k, v));
+		res.status(response.status).send(response.data);
+		logger.info("New c_nonce issued");
+	}
+	catch (e) {
+		logger.error(JSON.stringify(e));
+		res.status(500).send({ error: "internal_server_error" });
+	}
 });
 
 vciRouter.post('/credential', express.json(), async (req, res) => {
-	logger.info("New credential request received");
-	const response = await issuer.issueCredential({
-		request: {
-			headers: req.headers as any,
-			data: req.body,
-		},
-	});
+	try {
+		logger.info("New credential request received");
+		const response = await issuer.issueCredential({
+			request: {
+				headers: req.headers as any,
+				data: req.body,
+			},
+		});
 
-	logger.info("New credential has been issued");
-	Object.entries(response.headers).map(([k, v]) => res.setHeader(k, v));
-	res.status(response.status).send(response.data);
+		logger.info("New credential has been issued");
+		Object.entries(response.headers).map(([k, v]) => res.setHeader(k, v));
+		res.status(response.status).send(response.data);
+	}
+	catch (e) {
+		logger.error(JSON.stringify(e));
+		res.status(500).send({ error: "internal_server_error" });
+	}
 });
 
 vciRouter.post('/deferred-credential', express.json(), async (req, res) => {
-	logger.info("New deferred request received");
-	const response = await issuer.issueCredential({
-		request: {
-			headers: req.headers as any,
-			data: req.body,
-		},
-	});
+	try {
+		logger.info("New deferred request received");
+		const response = await issuer.issueCredential({
+			request: {
+				headers: req.headers as any,
+				data: req.body,
+			},
+		});
 
-	logger.info("Deferred response sent");
-	Object.entries(response.headers).map(([k, v]) => res.setHeader(k, v));
-	res.status(response.status).send(response.data);
+		logger.info("Deferred response sent");
+		Object.entries(response.headers).map(([k, v]) => res.setHeader(k, v));
+		res.status(response.status).send(response.data);
+	}
+	catch (e) {
+		logger.error(JSON.stringify(e));
+		res.status(500).send({ error: "internal_server_error" });
+	}
 });
 
 
 let metadata: any = undefined;
 
 vciRouter.get('/.well-known/openid-credential-issuer', async (_req, res) => {
-	if (!metadata) {
-		metadata = await issuer.getMetadata();
+	try {
+		if (!metadata) {
+			metadata = await issuer.getMetadata();
+		}
+		res.status(200).send(metadata);	
 	}
-	res.status(200).send(metadata);
+	catch (e) {
+		logger.error(JSON.stringify(e));
+		res.status(500).send({ error: "internal_server_error" });
+	}
 });	
 
