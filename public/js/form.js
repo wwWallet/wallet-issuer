3cd6a8d96604edb51ce37a1a2dfa95612524030e
gkatrakazas <grigoriskatrakazas@gmail.com>
2026-01-21 12:14:12 +0200

    remove old js files

--- public/js/form.js
+++
@@ -1,9 +0,0 @@
-function errorFeedback(id, timeout=3000, invalid="invalid", classes="") {
-	document.getElementById(id).className = `${classes} form-control invalid`;
-	document.getElementById(`${invalid}-${id}`).className = "error-feedback visible";
-
-	setTimeout(() => {
-		document.getElementById(id).className = `${classes} form-control`;
-		document.getElementById(`${invalid}-${id}`).className = "error-feedback";
-	}, timeout);
-}
