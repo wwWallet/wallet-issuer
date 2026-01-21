3a1392d221e614a0b42d304bf976e8646a0b3fc2
gkatrakazas <grigoriskatrakazas@gmail.com>
2026-01-21 12:49:29 +0200

    serve dynamic web manifest from config

--- src/app.ts
+++ src/app.ts
@@ -23,6 +23,35 @@ app.set('views', path.join(__dirname, '../../views'));
 
 app.use('/', landingRouter);
 
+app.get('/metadata/:filename', (req, res) => {
+	if (req.params.filename !== 'site.webmanifest') {
+		return res.status(404).send();
+	}
+	const manifest = {
+		name: config.siteConfig.name,
+		short_name: config.siteConfig.short_name,
+		start_url: "/",
+		display: "standalone",
+		background_color: config.siteConfig.background_color,
+		theme_color: config.siteConfig.theme_color,
+		icons: [
+			{
+				src: "/images/favicon-192x192.png",
+				sizes: "192x192",
+				type: "image/png"
+			},
+			{
+				src: "/images/favicon-512x512.png",
+				sizes: "512x512",
+				type: "image/png"
+			}
+		]
+	};
+
+	res.setHeader('Content-Type', 'application/manifest+json');
+	return res.send(manifest);
+});
+
 app.listen(config.port, () => {
 	console.log(`Nodejs service listening on ${config.url}`);
 });
