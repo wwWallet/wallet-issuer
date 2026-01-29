944057bcb7087583b714ffbd5a8cf369fd97f695
gkatrakazas <grigoriskatrakazas@gmail.com>
2026-01-29 16:04:00 +0200

    create a convert payload map function sd-jwt vc to mdoc

---
+++ src/lib/issuer/convertPidSdJwtVcToMdoc.ts
@@ -0,0 +1,53 @@
+type AnyObj = Record<string, any>;
+
+export function convertPidSdJwtVcToMdoc(pid: AnyObj) {
+	const address = (pid?.address ?? {}) as AnyObj;
+	const pob = (pid?.place_of_birth ?? {}) as AnyObj;
+	const ageEq = (pid?.age_equal_or_over ?? {}) as AnyObj;
+
+	return {
+		family_name: pid?.family_name,
+		family_name_birth: pid?.birth_family_name,
+		given_name: pid?.given_name,
+		given_name_birth: pid?.birth_given_name,
+
+		personal_administrative_number: pid?.personal_administrative_number,
+
+		birth_date: pid?.birthdate,
+
+		issuing_authority: pid?.issuing_authority,
+		issuing_country: pid?.issuing_country,
+		issuing_jurisdiction: pid?.issuing_jurisdiction,
+
+		document_number: pid?.document_number,
+
+		issuance_date: pid?.date_of_issuance,
+		expiry_date: pid?.date_of_expiry,
+
+		age_over_18: ageEq?.["18"],
+		age_over_21: ageEq?.["21"],
+		age_in_years: pid?.age_in_years,
+		age_birth_year: pid?.age_birth_year,
+
+		sex: pid?.sex,
+
+		nationality: pid?.nationalities,
+
+		birth_place: pob?.country,
+
+		resident_address: address?.formatted,
+		resident_country: address?.country,
+		resident_state: address?.region,
+		resident_city: address?.locality,
+		resident_postal_code: address?.postal_code,
+		resident_street: address?.street_address,
+		resident_house_number: address?.house_number,
+
+		portrait: pid?.picture,
+
+		email_address: pid?.email,
+		mobile_phone_number: pid?.phone_number,
+
+		trust_anchor: pid?.trust_anchor,
+	};
+}
